66669f0deeedcc503596c50533c959f9ec3cbb41
Abogeerick <abogeerick@gmail.com>
2023-06-14 15:56:25 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+require('./helpers.js');
+
+function moveDodgerLeft() {
+  const dodger = document.getElementById('dodger');
+  let left = dodger.style.left;
+  left = parseInt(left);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById('dodger');
+  let left = dodger.style.left;
+  left = parseInt(left);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
